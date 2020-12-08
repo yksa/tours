827cb3c98208d27b95aa1fc5d548ab7ed629355f
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-09 00:19:04 +0630

    loading screen added, if there is no data added refresh btn

--- src/Tour.js
+++ src/Tour.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Tour() {
+function Tour({ id, image, info, price, name, removeTour }) {
+    const [readMore, setReadMore] = useState(false);
     return (
-        <div>
-            <h2>tour component</h2>
-        </div>
-    )
+        <article className="single-tour">
+            <img src={image} alt={name} />
+            <footer>
+                <div className="tour-info">
+                    <h4>{name}</h4>
+                    <h4 className="tour-price">${price}</h4>
+                </div>
+                <p>
+                    {readMore ? info : `${info.substring(0, 200)}...`}
+                    <button onClick={() => setReadMore(!readMore)}>{readMore ? 'show less' : 'read more'}</button>
+                </p>
+                <button className="delete-btn" onClick={() => removeTour(id)}>not interested</button>
+            </footer>
+        </article>
+    );
 }
 
 export default Tour
--- src/Tours.js
+++ src/Tours.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Tour from './Tour';
 
-function Tours() {
+function Tours({ tours, removeTour }) {
     return (
-        <div>
-            <h2>tours component</h2>
-        </div>
+        <section>
+            <div className="title">
+                <h2>ours tours</h2>
+                <div className="underline"></div>
+            </div>
+            <div>
+                {tours.map(tour => {
+                    return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>
+                })}
+            </div>
+        </section>
     )
 }
 
